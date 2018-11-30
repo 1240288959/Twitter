712fb5eb3ba092b8c186ce8328e40605f0d383cb
tanyang <tanyang@jiuqi.com.cn>
2018-11-30 11:24:22 +0800

    添加过滤器

---
+++ twitter/src/main/java/com/tanyang/twitter/filter/LoginFilter.java
@@ -0,0 +1,28 @@
+package com.tanyang.twitter.filter;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@Component
+@ServletComponentScan
+@WebFilter(urlPatterns = "/login/*",filterName = "loginFilter")
+public class LoginFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
